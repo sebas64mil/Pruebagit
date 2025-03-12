9edce7a337e7c4438a287992f82e1c73e31b941c
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2025-03-11 21:56:46 -0500

    agrego comentarios

--- abogados.sql
+++ abogados.sql
@@ -3,11 +3,19 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
 
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
 USE `mydb` ;
 
-
+-- -----------------------------------------------------
+-- Table `mydb`.`EstadoDelCaso`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`EstadoDelCaso` (
   `idEstadoDelCaso` INT NOT NULL,
   `EstadoDelCasocol` VARCHAR(45) NOT NULL,
